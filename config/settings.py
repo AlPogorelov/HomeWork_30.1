7bbd522a1f81b075cca276b61cf79c630f85f9bc
AlPogorelov <eczempl@gmail.com>
2025-04-20 12:56:39 +0600

    Done

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,15 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+# Если используете .env локально, оберните это в условие
+if os.path.exists(".env"):
+    from dotenv import load_dotenv
+    load_dotenv()
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
