b9296072e76d7cb3802ba32657f52fa3695eabaf
AlPogorelov <eczempl@gmail.com>
2025-04-26 00:30:20 +0600

    Done

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.getenv('SECRET_KEY')
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # Если используете .env локально, оберните это в условие
 if os.path.exists(".env"):
